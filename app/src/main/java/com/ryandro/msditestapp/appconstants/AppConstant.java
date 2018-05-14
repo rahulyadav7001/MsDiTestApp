fcc9dc30d522b61d24c339f6b038889903022f29
rahulyadav7001 <rahulyadav7001@gmail.com>
2018-05-14 11:50:21 +0530

    Initial commit

---
+++ app/src/main/java/com/ryandro/msditestapp/appconstants/AppConstant.java
@@ -0,0 +1,26 @@
+package com.ryandro.msditestapp.appconstants;
+
+import android.graphics.Typeface;
+
+/**
+ * Created by WIN 1O on 15-03-2018.
+ */
+
+
+
+public class AppConstant {
+
+    // this class is use for whole application consatant attributes
+
+    // Typface Constant Start
+    public static Typeface REGULAR, BOLD, MEDIUM, LIGHT;
+
+    public static int HORIZONTAL_SPACING = DisplayUtil.dpToPx(4);
+
+    public static int VERTICAL_SPACING = DisplayUtil.dpToPx(8);
+
+    public static int HEIGHT_VIEW_REACTION = DisplayUtil.dpToPx(180);
+
+    public static final int MAX_ALPHA = 170;
+
+}
